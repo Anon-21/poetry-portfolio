8294c9eeb7d2686b7d05fdc1a5bc121edbfa4eee
Anon-21 <sonal.mohrir@gmail.com>
2026-03-30 19:35:05 +0530

    skeleton

---
+++ app/poems/[slug]/loading.tsx
@@ -0,0 +1,18 @@
+import Skeleton from "react-loading-skeleton"
+import "react-loading-skeleton/dist/skeleton.css"
+
+export default function Loading() {
+  return (
+    <main className="max-w-2xl mx-auto p-10">
+      <Skeleton
+  height={40}
+  width="60%"
+  baseColor="var(--bg-cream)"
+  highlightColor="#fdf6ec"
+  className="mb-6" 
+/>
+
+      <Skeleton count={8} className="mb-2" />
+    </main>
+  )
+}
